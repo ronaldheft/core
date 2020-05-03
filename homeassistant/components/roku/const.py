9f290b501ed1f8254b91d259636c4de9fdf1487d
Chris Talkington <chris@talkingtontech.com>
2020-05-03 10:25:58 -0500

    Update const.py

--- homeassistant/components/roku/const.py
+++ homeassistant/components/roku/const.py
@@ -7,8 +7,5 @@ ATTR_MANUFACTURER = "manufacturer"
 ATTR_MODEL = "model"
 ATTR_SOFTWARE_VERSION = "sw_version"
 
-DATA_CLIENT = "client"
-DATA_DEVICE_INFO = "device_info"
-
+# Default Values
 DEFAULT_PORT = 8060
-DEFAULT_MANUFACTURER = "Roku"
