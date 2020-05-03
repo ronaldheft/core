8129fb796b1b32e435a2cd356c785aee119f1922
Chris Talkington <chris@talkingtontech.com>
2020-05-03 10:23:13 -0500

    Update const.py

--- homeassistant/components/roku/const.py
+++ homeassistant/components/roku/const.py
@@ -1,6 +1,12 @@
 """Constants for the Roku integration."""
 DOMAIN = "roku"
 
+# Attributes
+ATTR_IDENTIFIERS = "identifiers"
+ATTR_MANUFACTURER = "manufacturer"
+ATTR_MODEL = "model"
+ATTR_SOFTWARE_VERSION = "sw_version"
+
 DATA_CLIENT = "client"
 DATA_DEVICE_INFO = "device_info"
 
