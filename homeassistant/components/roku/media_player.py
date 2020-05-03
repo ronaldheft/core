e09699a5a7d2972fd89a8048e328151073f43427
Chris Talkington <chris@talkingtontech.com>
2020-05-03 11:45:12 -0500

    Update media_player.py

--- homeassistant/components/roku/media_player.py
+++ homeassistant/components/roku/media_player.py
@@ -19,6 +19,7 @@ from homeassistant.components.media_player.const import (
 )
 from homeassistant.const import STATE_HOME, STATE_IDLE, STATE_PLAYING, STATE_STANDBY
 
+from . import RokuDataUpdateCoordinator, RokuEntity
 from .const import DEFAULT_MANUFACTURER, DEFAULT_PORT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -38,9 +39,9 @@ SUPPORT_ROKU = (
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the Roku config entry."""
-    roku = hass.data[DOMAIN][entry.entry_id]
+    coordinator = hass.data[DOMAIN][entry.entry_id]
     unique_id = roku.device.info.serial_number
-    async_add_entities([RokuMediaPlayer(unique_id, roku)], True)
+    async_add_entities([RokuMediaPlayer(unique_id, coordinator)], True)
 
 
 class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
@@ -54,45 +55,26 @@ class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
             device_id=unique_id,
         )
 
-        self._available = False
-        self._channels = []
-        self._channel_ids = []
-
-    async def async_update(self) -> None:
-        """Retrieve latest state."""
-        try:
-            await self.roku.update()
-            self._available = True
-            self._channels = self.get_source_list()
-            self._channel_ids = {app.name: app.app_id for app in self.roku.device.apps}
-        except RokuError:
-            self._available = False
-
     def get_source_list(self) -> List:
         """Get the list of applications to be used as sources."""
         return ["Home"] + sorted(app.name for app in self.roku.device.apps)
 
     @property
-    def should_poll(self) -> bool:
-        """Device should be polled."""
-        return True
-
-    @property
     def state(self) -> str:
         """Return the state of the device."""
-        if self.roku.device.state.standby:
+        if self.coordinator.data.state.standby:
             return STATE_STANDBY
 
-        if self.roku.device.app is None:
+        if self.coordinator.data.app is None:
             return None
 
-        if self.roku.device.app.name == "Power Saver" or self.roku.device.app.screensaver:
+        if self.coordinator.data.app.name == "Power Saver" or self.roku.device.app.screensaver:
             return STATE_IDLE
 
-        if self.roku.device.app.name == "Roku":
+        if self.coordinator.data.app.name == "Roku":
             return STATE_HOME
 
-        if self.roku.device.app.name is not None:
+        if self.coordinator.data.app.name is not None:
             return STATE_PLAYING
 
         return None
@@ -103,14 +85,9 @@ class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
         return SUPPORT_ROKU
 
     @property
-    def available(self) -> bool:
-        """Return if able to retrieve information from device or not."""
-        return self._available
-
-    @property
     def media_content_type(self) -> str:
         """Content type of current playing media."""
-        if self.roku.device.app is None or self.roku.device.app.name in ("Power Saver", "Roku"):
+        if self.coordinator.data.app is None or self.coordinator.data.app.name in ("Power Saver", "Roku"):
             return None
 
         return MEDIA_TYPE_CHANNEL
@@ -118,10 +95,10 @@ class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
     @property
     def media_image_url(self) -> str:
         """Image url of current playing media."""
-        if self.roku.device.app is None or self.roku.device.app.name in ("Power Saver", "Roku"):
+        if self.coordinator.data.app is None or self.coordinator.data.app.name in ("Power Saver", "Roku"):
             return None
 
-        if self.roku.device.app.id is None:
+        if self.coordinator.data.app.id is None:
             return None
 
         return self.roku.app_icon_url(self.roku.device.app.id)
@@ -130,7 +107,7 @@ class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
     def app_name(self) -> str:
         """Name of the current running app."""
         if self.roku.device.app is not None:
-            return self.roku.device.app.name
+            return self.coordinator.data.app.name
 
         return None
 
@@ -153,7 +130,7 @@ class RokuMediaPlayer(RokuEntity, MediaPlayerEntity):
     @property
     def source_list(self) -> List:
         """List of available input sources."""
-        return self._channels
+        return self.get_source_list()
 
     async def async_turn_on(self) -> None:
         """Turn on the Roku."""
